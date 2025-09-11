0fd3f694bca83c3111be537acec02667d465f107
Jo-Pimentel <joaopodolsky59@gmail.com>
2025-09-11 19:21:55 -0300

    Por enquanto, última atualização grande desse projeto. Houve mudança no tipo de elemento do visor da calculadora, passando de <div> para <input>, com o objetivo dos números não vazarem dele, caso muito grandes. Por isso, tive que mudar algumas coisas do código fonte, como o método 'textContent' do antigo visor, que agora foram mudados por 'value'.

--- javascript/script_calculadora.js
+++ javascript/script_calculadora.js
@@ -1,4 +1,4 @@
-const conteudo_visor_da_calculadora = document.querySelector("#conteudo_visor_da_calculadora");
+const visor_da_calculadora = document.querySelector("#visor_da_calculadora");
 
 const numeros_disponiveis = [...document.querySelectorAll(".numero_disponivel")];
 
@@ -12,22 +12,22 @@ let primeiro_numero, segundo_numero, acao_escolhida;
 
 numeros_disponiveis.map((numero_disponivel) => {
     numero_disponivel.addEventListener("click", () => {
-        if(conteudo_visor_da_calculadora.textContent.length < 6) {
-            conteudo_visor_da_calculadora.textContent += numero_disponivel.textContent;
-        } else {
-            alert("O número não pode ter mais de seis dígitos.")
-        }
+        visor_da_calculadora.disabled = false;
+
+        visor_da_calculadora.value += numero_disponivel.textContent;
+
+        visor_da_calculadora.disabled = true;
     })
 })
 
 function definir_acao_e_verificar_valores_existentes(botao_selecionado) {
     if(!acao_escolhida) {
         if(!primeiro_numero) {
-            primeiro_numero = parseFloat(conteudo_visor_da_calculadora.textContent);
+            primeiro_numero = parseFloat(visor_da_calculadora.value);
                 
             acao_escolhida = botao_selecionado.textContent;
 
-            conteudo_visor_da_calculadora.textContent = "";
+            visor_da_calculadora.value = "";
         }
     }
 }
@@ -48,19 +48,19 @@ botoes_complementares.map((botao_complementar) => {
     botao_complementar.addEventListener("click", () => {
         switch(botao_complementar.textContent) {
             case ",":
-                if(conteudo_visor_da_calculadora.textContent != "") {
-                    if(!conteudo_visor_da_calculadora.textContent.includes(".")) {
-                        conteudo_visor_da_calculadora.textContent += ".";
+                if(visor_da_calculadora.value != "") {
+                    if(!visor_da_calculadora.value.includes(".")) {
+                        visor_da_calculadora.value += ".";
                     }
                 }
             break
 
             case "C":
-                conteudo_visor_da_calculadora.textContent = "";
+                visor_da_calculadora.value = "";
             break
 
             case "CA":
-                conteudo_visor_da_calculadora.textContent = "";
+                visor_da_calculadora.value = "";
 
                 resetar_variaveis_de_numero_e_acao();
             break
@@ -75,39 +75,39 @@ botoes_complementares.map((botao_complementar) => {
 
 botao_resultado.addEventListener("click", () => {
     if(!segundo_numero && primeiro_numero) {
-        if(conteudo_visor_da_calculadora.textContent != "") {
-            segundo_numero = parseFloat(conteudo_visor_da_calculadora.textContent);
+        if(visor_da_calculadora.value != "") {
+            segundo_numero = parseFloat(visor_da_calculadora.value);
             
             switch(acao_escolhida) {
                 case "+":
-                    conteudo_visor_da_calculadora.textContent = primeiro_numero + segundo_numero;
+                    visor_da_calculadora.value = primeiro_numero + segundo_numero;
                 break
 
                 case "-":
-                    conteudo_visor_da_calculadora.textContent = primeiro_numero - segundo_numero;
+                    visor_da_calculadora.value = primeiro_numero - segundo_numero;
                 break
 
                 case "x":
-                    conteudo_visor_da_calculadora.textContent = primeiro_numero * segundo_numero;
+                    visor_da_calculadora.value = primeiro_numero * segundo_numero;
                 break
 
                 case "÷":
-                    conteudo_visor_da_calculadora.textContent = primeiro_numero / segundo_numero;
+                    visor_da_calculadora.value = primeiro_numero / segundo_numero;
                 break
 
                 case "%":
-                    conteudo_visor_da_calculadora.textContent = (primeiro_numero / 100) * segundo_numero;
+                    visor_da_calculadora.value = (primeiro_numero / 100) * segundo_numero;
                 break
 
                 default:
                     if(primeiro_numero > segundo_numero) {
-                        conteudo_visor_da_calculadora.textContent = primeiro_numero;
+                        visor_da_calculadora.value = primeiro_numero;
                     } else {
-                        conteudo_visor_da_calculadora.textContent = segundo_numero;
+                        visor_da_calculadora.value = segundo_numero;
                     }
                 break
             }
-
+            
             resetar_variaveis_de_numero_e_acao();
         }
     }
